dbb6c591658272bfd2155c51b8b278f22fe3f2de
bdksdk <leman.ibrahimli23@gmail.com>
2021-02-20 15:36:24 +0400

    all

--- vendors/main/kurs-python-dersler.js
+++ vendors/main/kurs-python-dersler.js
@@ -10,23 +10,26 @@ fetch('http://161.97.113.89/api/v1/topic/?course_id='+course_id)
     for(let i=0;i<json.length;i++){
       let bv=json[i].id
       let br=json[i].name
-      let coursetopic=document.getElementById('coursetopic')
+      let coursetopicdata=document.getElementById('coursetopicdata')
+
       fetch('http://161.97.113.89/api/v1/pod_topic/?topic_id='+bv)
       .then((response) => response.json())
       .then((data) =>{ 
         let out=[]
         let fgf=document.getElementById("fgf")
         data.forEach(element => out.push(`${element.name}`));
-        for(let t=0;t<out.length;t++){ console.log(out[t])}
-          coursetopic.innerHTML+=
+        // for(let t=0;t<out.length;t++){ console.log(out[t])}
+          coursetopicdata.innerHTML+=
           `
           <button class="accordion" id="dataAccardionname1">${br}</button>
-          <div class="panel" >
-          <div class="row" style="padding: 5px 16px; width: 600px">
-            <div class="col-1"><img src="./img/courses/movzular.svg" alt="" class="img-card"></div>
-            <div class="col-5" id="hhhhhh"><a class="paner_into_button" href="${x}">${out}Bax</a></div>       
-          </div>
-          <hr>
+          <div class="panel_2">
+            <div class="container-fluid" style="padding-top:5px">
+              <div class="row">
+                <div class="col-3 cheachboxclass">
+                  <input type="checkbox" id="checkboxId" onclick="clickCheckbox2()" class="padding_element"></div>
+                <div class="col-6" id="datacorse2">${out}</div>
+              </div>
+            </div>
           </div>
           `
         var acc = document.getElementsByClassName("accordion");
@@ -42,11 +45,32 @@ fetch('http://161.97.113.89/api/v1/topic/?course_id='+course_id)
         } 
         });
         }
-      });
+      });  
     }
   });
 
 
+  var i = 0,images = ["http://161.97.113.89/api/v1/content/?topic_id="+24];
+                            
+  function mySlide(param)
+  {
+      if(param === 'next')
+      {
+          i++;
+          
+          if(i === images.length){ i = images.length - 1; }
+      }else{
+          i--;
+          if(i < 0){ i = 0; }
+      }
+      fetch(images[i])
+  .then((response) => response.json())
+  .then(function (json) {
+    document.getElementById('slide').innerHTML = json[0].id;
+    document.getElementById('slide1').innerHTML = json[0].text;
+
+})
+  }
 
 
 
